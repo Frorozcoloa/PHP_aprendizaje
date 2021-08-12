787a0f60edb15b2c8a525eebba7699d6e8095b43
Fredy <freorozcoloa@gmail.com>
2021-08-11 23:40:26 -0500

    Semana 4 acabada

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <title>Fredy Orozco (60f67dd2)</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to Autos Database</h1>
+        </p>
+    <p><a href="login.php">Please Log In</a></p>
+        <a href="https://www.wa4e.com/assn/autosdb/" target="_blank">Specification for this Application</a>
+    </div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,79 @@
+<?php
+    session_start();
+    require_once 'PDO.php';
+
+ 
+
+    $salt = 'XyZzy12*_';
+    $stored_hash = hash('md5', 'XyZzy12*_php123');
+
+    if(isset($_POST['cancel'])){
+        header("Location: index.php");
+        return;
+    }
+
+    if(isset($_POST['who']) && (isset($_POST['pass']))){
+        if(strlen($_POST['who'])<1 || strlen($_POST['pass']<1)){
+            $_SESSION['error'] = "User name and password are required";
+            header("Location:login.php");
+            return;
+        }
+        else if(strpos($_POST['who'], "@") === false){
+            $_SESSION['error']  = "Email must have an at-sign (@)";
+            header("Location:login.php");
+            return;
+        }
+        else{
+            $hash_md5 = hash('md5', $salt.$_POST['pass']);
+            if($hash_md5 == $stored_hash){
+                $_SESSION['name'] =$_POST['who'];
+                error_log("Login success ".$_POST['who']);
+                header("Location:view.php");
+                return;
+
+                
+            }
+            else{
+                $_SESSION['error']  = "Incorrect password";
+                error_log("Login fail ".$_POST['who']." $hash_md5");
+                header("Location:login.php");
+                return;
+
+            }
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Fredy Orozco (60f67dd2)'s Login Page</title>
+    <?php require_once 'bootrap.php'; ?>
+</head>
+<body>
+    <div class="container">
+        <h1>Please login</h1>
+        <?php
+            if ( isset($_SESSION['error']) ) {
+            echo('<p style="color: red;">'.htmlentities($_SESSION['error'])."</p>\n");
+            unset($_SESSION['error']);
+            }
+    ?>
+        <form method="post">
+            <label for="name">email</label>
+            <input type="text" name="who" id="name">
+            <br>
+            <label for="id_1723">Password</label>
+            <input type="text" name="pass" id="id_1723">
+            <input type="submit" value="Log In">
+            <input type="submit" value="Cancel" name="cancel">
+        </form>
+        <p>
+        For a password hint, view source and find a password hint
+        in the HTML comments.
+        <!-- Hint: The password is the four character sound a cat
+        makes (all lower case) followed by 123. -->
+    </p>
+    </div>
+</body>
+</html>
