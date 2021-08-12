96c4febad451e5168c0558cf76437dfc8b69bebf
Fredy <freorozcoloa@gmail.com>
2021-08-12 11:47:13 -0500

    Todo se hizo#

--- index.php
+++ index.php
@@ -1,17 +1,88 @@
+<?php 
+    session_start();
+    require_once  'PDO.php';
+
+    if(!isset($_SESSION['name'])){
+        die("Not Logged in");
+
+    }
+    
+$stmt = $pdo->query("SELECT * FROM autos");
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-    <title>Fredy Orozco (60f67dd2)</title>
+    <title>Fredy Alberto Orozco Loaiza  Login Page</title>
+    <?php  require_once 'bootrap.php'?>
 </head>
 <body>
     <div class="container">
-        <h1>Welcome to Autos Database</h1>
-        </p>
-    <p><a href="login.php">Please Log In</a></p>
-        <a href="https://www.wa4e.com/assn/autosdb/" target="_blank">Specification for this Application</a>
-    </div>
+        <h1> Tracking Autos for <?php echo($_SESSION['name'])?></h1>
+        <?php 
+        if ( isset($_SESSION['success']) ){
+            echo('<p style="color: green;">'.htmlentities($_SESSION['success'])."</p>\n");
+            unset($_SESSION['success']);
+        }
+        ?>
+        </form>
+        <h2>Automobiles</h2>
+        <?php
+    if (isset($_SESSION['success'])) {
+        echo('<p style="color: green;">' . htmlentities($_SESSION['success']) . "</p>\n");
+        unset($_SESSION['success']);
+    }
+    if (isset($_SESSION['error'])) {
+        echo('<p style="color: red;">' . htmlentities($_SESSION['error']) . "</p>\n");
+        unset($_SESSION['error']);
+    }
+    ?>
+
+    
+
+    <ul>
+
+        <?php
+        if (isset($_SESSION['name'])) {
+            if (sizeof($rows) > 0) {
+                echo "<table border='1'>";
+                echo " <thead><tr>";
+                echo "<th>Make</th>";
+                echo " <th>Model</th>";
+                echo " <th>Year</th>";
+                echo " <th>Mileage</th>";
+                echo " <th>Action</th>";
+                echo " </tr></thead>";
+                foreach ($rows as $row) {
+                    echo "<tr><td>";
+                    echo(htmlentities($row['make']));
+                    echo("</td><td>");
+                    echo(htmlentities($row['model']));
+                    echo("</td><td>");
+                    echo(htmlentities($row['year']));
+                    echo("</td><td>");
+                    echo(htmlentities($row['mileage']));
+                    echo("</td><td>");
+                    echo('<a href="edit.php?auto_id='.$row['auto_id'].'">Edit</a> / <a href="delete.php?auto_id='.$row['auto_id'].'">Delete</a>');
+                    echo("</td></tr>\n");
+                }
+                echo "</table>";
+            } else {
+                echo 'No rows found';
+            }
+            echo '</li><br/></ul>';
+            echo '<p><a href="add.php">Add New Entry</a></p>
+    <p><a href="logout.php">Logout</a></p><p>
+        <b>Note:</b> Your implementation should retain data across multiple
+        logout/login sessions.  This sample implementation clears all its
+        data on logout - which you should not do in your implementation.
+    </p>';
+        } else {
+
+            echo "<p><a href='login.php'>Please log in</a></p><p>Attempt to <a href='add.php'>add data</a> without logging in</p>";
+        } ?>
+        <p><a href="add.php">Add New</a>|<a href="logout.php">logout</a></p>
+        </div>
 </body>
 </html>
